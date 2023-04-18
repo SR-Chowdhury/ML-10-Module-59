cc7752c37b8c21ddea5705014c03467986eb0c6a
Shihan Chowdhury <hackedgdown@gmail.com>
2023-04-18 01:42:33 +0100

    59-2 Simple form, input, change, blur, submit, preventDefault done

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        // console.log(email, password)
+        // setEmail(email);
+        // setPassword(password);
+    }
+
     return (
         <div>
             <h1 className='my-5'>This is Register Page</h1>
             <div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input className='form-control' type="email" id='email' placeholder='Enter Your email' />
-                    <input className='form-control my-3' type="password" placeholder='******' />
+                    <input className='form-control my-3' type="password" id='password' placeholder='******' />
                     <input className='form-control bg-info' type="submit" value="Register" />
                 </form>
             </div>
